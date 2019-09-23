1520a9f59154332f82ede0a8eeb12fb75ffd1290
Max Lee <maxolee23@gmail.com>
2019-09-23 20:12:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,9 +28,13 @@ class Song
   end
 
   def self.create_by_name(title)
-    song = self.create 
-    song.name = title 
-    song 
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
   end
 
 end
